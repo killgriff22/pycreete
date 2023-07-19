3c35408a52f433a4bf3eb1541a3007e401b5ed0c
griffn <37548204+killgriff22@users.noreply.github.com>
2023-07-19 08:50:25 -0400

    Update pymint.py

--- src/pymint/pymint.py
+++ src/pymint/pymint.py
@@ -1,4 +1,51 @@
-defualtexpect="1234567890 Ω≈ç√∫˜µ≤≥÷åß∂ƒ©˙∆˚¬…æœ∑´®†¥¨ˆø“‘«`¡™£¢∞§¶•ªº–≠¸˛Ç◊ı˜¯˘¿ÅÍÎÏ˝ÓÔÒÚÆŒ„´‰ˇÁ¨ˆØ∏”’»`⁄€‹›ﬁﬂ‡°·‚—±ZXCVBNM<>?ASDFGHJKL:QWERTYUIOP}[|~!@#$%^&*()_+zxcvbnm,./asdfghjkl;qwertyuiop[]"
+defualtexpect="1234567890ZXCVBNM<>?ASDFGHJKL:QWERTYUIOP}[|~!@#$%^&*()_+zxcvbnm,./asdfghjkl;qwertyuiop[]"
+#very proud of this very shitty encryption method i wrote like a year ago
+import random
+class newmethod:
+    def encode_string(string,rot,seed):
+        strng =""
+        if type(seed) == str:
+            seed_i=0
+            for char in seed:
+                seed_i+=ord(char)
+            seed=seed_i
+            del seed_i
+        for i,char in enumerate(string):
+            shift = random.randint(0,100)
+            nextshift = 0 if i == 0 else random.randint(0,100)
+            next=f"{hex(int(shift*rot+nextshift)*seed).split('0x')[1]}​{hex(int(rot+ord(char)+shift)*seed).split('0x')[1]}​{hex(int(nextshift*rot)*seed).split('0x')[1]}​"
+            strng += next
+        return strng
+    def decode_string(string,rot,seed):
+        strng = ""
+        split = string.split("​")
+        lst=[int(split[i],16) for i in range(len(split)-1)]
+        split=lst
+        del(lst)
+        if type(seed) == str:
+            seed_i=0
+            for char in seed:
+                seed_i+=ord(char)
+            seed=seed_i
+            del seed_i
+        for i in range(0,len(split)-1,3):
+            _ = int(split[i])
+            __ = int(split[i+1])
+            ___ = int(split[i+2])
+            ___ //= seed
+            ___ //= rot
+            _ //= seed
+            _ -= ___
+            _ //= rot
+            #_ += seed
+            __ //= seed
+            __ -= (rot+_)
+            #__+=seed*rot
+            try:
+                strng+=chr(__)
+            except:
+                print("Failed to decode")
+        return strng
 class x4x5:
     def generatekey(expect=defualtexpect):
         import random
